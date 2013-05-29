346ecd15c2d995a275f027a65e93cbb743b91502
Cyril <cyril.labbe@gmail.com>
2013-05-30 00:49:48 +0200

    silent autoloader

--- includes/main/autoload.inc.php
+++ includes/main/autoload.inc.php
@@ -12,12 +12,7 @@ function __autoload($class_name) {
 	}
 	$path .= '.class.php';
 
-	if (file_exists($path) === false) {
-		var_dump("Failed to open <b>'" . $class_name . "'</b> class file :" . $path/*, E_USER_WARNING*/);
-	} else {
+	if (file_exists($path) === true) {
 		include_once $path;
-		if (class_exists($class_name, false) === false) {
-			var_dump("Failed to autoload class " . $class_name/*, E_USER_ERROR*/);
-		}
 	}
 }
